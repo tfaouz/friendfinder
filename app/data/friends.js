7bb76bd23a69d061c7dcf6dbcac6b7fe93d4aa79
Thibault <ThibaultFaouz@gmail.com>
2019-06-12 14:13:51 -0400

    friends serious

---
+++ app/data/friends.js
@@ -0,0 +1,157 @@
+var friends = [
+
+    {
+        "name": "Steve",
+        "photo": "https://parade.com/wp-content/uploads/2017/09/montel-williams-new-series-ftr.jpg",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "4",
+            "1",
+            "1",
+            "4",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+
+    {
+        "name": "Lebron",
+        "photo": "https://a3.espncdn.com/combiner/i?img=%2Fphoto%2F2018%2F0930%2Fr440005_1296x729_16%2D9.jpg",
+        "scores": [
+            "2",
+            "1",
+            "3",
+            "4",
+            "5",
+            "3",
+            "2",
+            "5",
+            "5",
+            "2"
+        ]
+    },
+
+    {
+        "name": "Mark",
+        "photo": "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTQyMDA0NDgwMzUzNzcyNjA2/mark-zuckerberg_gettyimages-512304736jpg.jpg",
+        "scores": [
+            "3",
+            "3",
+            "2",
+            "3",
+            "3",
+            "3",
+            "2",
+            "4",
+            "4",
+            "2"
+        ]
+    },
+
+    {
+        "name": "Sophie",
+        "photo": "https://s.yimg.com/ny/api/res/1.2/y3bVnns51_5OnOiN4Ei9AA--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/http://media.zenfs.com/en-US/homerun/people_218/579d63caf693ad5590d10a216840d727",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "5",
+            "3",
+            "3",
+            "5",
+            "2"
+        ]
+    },
+
+    {
+        "name": "Mary",
+        "photo": "https://media.wmagazine.com/photos/5c87dd696a6e282c9f21b66e/4:3/w_1536/GettyImages-967564850.jpg",
+        "scores": [
+            "2",
+            "1",
+            "1",
+            "1",
+            "5",
+            "1",
+            "2",
+            "5",
+            "1",
+            "2"
+        ]
+    },
+
+    {
+        "name": "Leonardo",
+        "photo": "http://d17zbv0kd7tyek.cloudfront.net/wp-content/uploads/2015/06/leonardo-dicaprio-fb.jpg",
+        "scores": [
+            "3",
+            "3",
+            "5",
+            "4",
+            "5",
+            "5",
+            "4",
+            "5",
+            "4",
+            "2"
+        ]
+    },
+
+    {
+        "name": "Oliver",
+        "photo": "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTU3NDI3NzMzNjgzNTEyNTIy/gettyimages-9781796821.jpg",
+        "scores": [
+            "2",
+            "1",
+            "3",
+            "4",
+            "5",
+            "3",
+            "2",
+            "5",
+            "5",
+            "2"
+        ]
+    },
+
+    {
+        "name": "Cedric",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/CedricEntertainerMarch04.jpg/220px-CedricEntertainerMarch04.jpg",
+        "scores": [
+            "5",
+            "1",
+            "5",
+            "4",
+            "4",
+            "3",
+            "3",
+            "3",
+            "1",
+            "2"
+        ]
+    },
+
+    {
+        "name": "Matthew",
+        "photo": "https://timedotcom.files.wordpress.com/2016/12/matthew-mcconaughey.jpg",
+        "scores": [
+            "5",
+            "1",
+            "5",
+            "4",
+            "4",
+            "3",
+            "5",
+            "2",
+            "2",
+            "2"
+        ]
+    }
+];
+
+module.exports = friends;
