a998e558e504b930b7ad9c328b9f14aa16eea911
Thibault <ThibaultFaouz@gmail.com>
2019-06-12 15:43:00 -0400

    should be good to go

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 8080;
 
-var jsonParser = bodyParser.json()
+//var jsonParser = bodyParser.json()
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.json({ type: 'application/*+json' }));
 
-app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
 
-app.use(bodyParser.text({ type: 'text/html' }))
+app.use(bodyParser.text({ type: 'text/html' }));
 
 //enables api routes to connect//
 require("./app/routing/api-routes.js")(app)
