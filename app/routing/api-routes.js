a5083f0739141a81d263b8e6b6aa741c56d6b187
Thibault <ThibaultFaouz@gmail.com>
2019-06-12 14:30:24 -0400

    had video help

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -9,5 +9,35 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
 
+        var bestFriendMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+        // console.log(req.body); //
+        var userData = req.body;
+        var userScores = userData.scores;
+        // console.log(userScores) //
+
+        var totalDifference = 0;
+
+        for (var i = 0; i < friends.length; i++) {
+            // console.log(friends[i].name); //
+            totalDifference = 0;
+
+            for (var j = 0; j < friends[i].scores[j]; j++) {
+
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+                // finds difference between match //
+                if (totalDifference <= bestFriendMatch.friendDifference) {
+
+                    bestFriendMatch.name = friends[i].name;
+                    bestFriendMatch.photo = friends[i].photo;
+                    bestFriendMatch.friendDifference = totalDifference;
+                }
+            }
+        }
+        friends.push(userData);
+        res.json(bestFriendMatch);
     });
 }
