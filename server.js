2cb2abc8bbc424fbea6fc358da8b2d633b0a6787
Thibault <ThibaultFaouz@gmail.com>
2019-06-12 04:59:53 -0400

    start

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var PORT = process.env.PORT || 8080;
+
+var jsonParser = bodyParser.json()
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+
+app.use(bodyParser.text({ type: 'text/html' }))
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
