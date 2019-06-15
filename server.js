08bdde8bb8c7d595d90ca518eb2689c66869f9bf
Thibault <ThibaultFaouz@gmail.com>
2019-06-14 21:03:52 -0400

    works finally

--- server.js
+++ server.js
@@ -4,20 +4,13 @@ var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 8080;
 
-//var jsonParser = bodyParser.json()
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/*+json' }));
-
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
-
 app.use(bodyParser.text({ type: 'text/html' }));
 
-//enables api routes to connect//
 require("./app/routing/api-routes.js")(app)
 require("./app/routing/html-routes.js")(app)
-// enables html routes to connect to server //
 
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
